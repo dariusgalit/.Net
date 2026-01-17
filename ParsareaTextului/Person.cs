f9b3a085d49a5c7f6fababe56bbaa299d7d83cff
dariusgalit <149113085+dariusgalit@users.noreply.github.com>
2026-01-17 19:04:48 +0200

    Add files via upload

---
+++ ParsareaTextului/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Person
+{
+    public string Prenume { get; set; }
+    public string Nume { get; set; }
+    public string Oras { get; set; }
+
+    public Person(string prenume, string nume, string oras)
+    {
+        Prenume = prenume;
+        Nume = nume;
+        Oras = oras;
+    }
+
+    public void Afisare()
+    {
+        Console.WriteLine($"{Prenume} {Nume} {Oras}");
+    }
+}
