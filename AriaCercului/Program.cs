5f42325a82699124abfb07cc03f96f3cbca3e31f
dariusgalit <149113085+dariusgalit@users.noreply.github.com>
2026-02-16 21:46:23 +0200

    Add files via upload

---
+++ AriaCercului/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+    
+Func<double, string> areaMessage = r => $"Aria cercului cu raza {r} este {Math.PI * r * r:F3}.";
+
+Console.Write("Introduceți raza cercului: ");
+if (double.TryParse(Console.ReadLine(), out var radius) && radius >= 0)
+{
+    Console.WriteLine(areaMessage(radius));
+}
+else
+{
+    Console.WriteLine("Valoare invalidă pentru rază.");
+}
